0d676eea61bd0b3f693fd34f8b23c059ed29351f
Egor <borland8@yandex.ru>
2025-09-23 12:44:27 +0300

    update

--- modules/core/src/com/company/testapp/core/ReportInitializer.java
+++ modules/core/src/com/company/testapp/core/ReportInitializer.java
@@ -1,21 +1,14 @@
 package com.company.testapp.core;
 
-import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.Resources;
-import com.haulmont.cuba.core.global.UserSessionSource;
-import com.haulmont.cuba.core.sys.AppContext;
-import com.haulmont.cuba.core.sys.SecurityContext;
 import com.haulmont.cuba.core.sys.events.AppContextStartedEvent;
 import com.haulmont.cuba.security.app.Authenticated;
-import com.haulmont.cuba.security.app.Authentication;
-import com.haulmont.cuba.security.global.UserSession;
 import com.haulmont.reports.ReportImportExportAPI;
 import org.apache.poi.util.IOUtils;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
@@ -24,19 +17,14 @@ import java.util.List;
 public class ReportInitializer {
 
     @Inject
-    ReportImportExportAPI reportImportExportAPI;
+    private ReportImportExportAPI reportImportExportAPI;
 
     @Inject
-    Resources resources;
-
-    @Inject
-    private Authentication authentication;
+    private Resources resources;
 
     @Authenticated
     @EventListener
     public void onAppStarted(AppContextStartedEvent event) {
-        SecurityContext securityContext = AppContext.getSecurityContext();
-        authentication.begin();
         List<String> reportFiles = Arrays.asList("com/company/testapp/core/resources/reports/Отчет по учителям.zip",
                 "com/company/testapp/core/resources/reports/Отчет по количеству учеников.zip");
 
@@ -45,20 +33,10 @@ public class ReportInitializer {
                 if (reportStream != null) {
                     byte[] reportsZip = IOUtils.toByteArray(reportStream);
                     reportImportExportAPI.importReports(reportsZip);
-                } else {
-                    System.out.println("Не найден файл отчёта: " + reportPath);
                 }
             } catch (Exception e) {
                 throw new RuntimeException("Ошибка при импорте отчёта: " + reportPath, e);
-            }finally {
-                authentication.end();
             }
         }
     }
 }
-
-//        private void importReportsIntoProject(String reportFiles) throws IOException{
-//
-//            InputStream reportStream = getClass().getResourceAsStream(reportFiles);
-//        }
-//}
